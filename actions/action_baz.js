e8e95b5f19b0825fd1b4a3a76930061c0511f195
DataFire <info@datafire.io>
2017-12-22 13:11:21 -0500

    delete actions

--- actions/action_baz.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let datafire = require('datafire');
-
-module.exports = new datafire.Action({
-  handler: async (input, context) => {
-    let result = await Promise.resolve();
-    return result;
-  },
-});
