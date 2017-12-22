685d42bc50de206d082ea3cf4ce76a925fd2d89a
DataFire <info@datafire.io>
2017-12-22 13:09:49 -0500

    add foo

---
+++ actions/action_foo.js
@@ -0,0 +1,12 @@
+"use strict";
+let datafire = require('datafire');
+
+module.exports = new datafire.Action({
+  handler: async (input, context) => {
+    
+    let result2 = await Promise.resolve();
+    let result = await Promise.resolve();
+    let result1 = await Promise.resolve();
+    return result;
+  },
+});
